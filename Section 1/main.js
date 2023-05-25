0f84d52a672a404de1ade8ab16a955c01b9016fa
Kamola0911 <kamola092002@mail.ru>
2023-05-25 18:01:21 +0500

    js problems added

---
+++ Section 1/main.js
@@ -0,0 +1,82 @@
+// --- Boolean-1 ---
+// let A = 5;
+
+// console.log(A%2 == 0);
+
+// --- Boolean-2 ---
+// let A = 5;
+// let B = 8;
+// console.log(A >= 0 || B <-2);
+
+// --- Boolean-3 ---
+// let A = 5;
+// let B = 8;
+// let C = 4;
+// console.log(A < B < C);
+
+// --- Boolean-4 ---
+// let A = 5;
+// let B = 8;
+// console.log((A % 2 == 1) && (B % 2 == 1));
+
+// --- Boolean-5 ---
+// let A = 5;
+// let B = 8;
+// console.log((A % 2 == 1) || (B % 2 == 1));
+
+// --- Boolean-6 ---
+// let A = 5;
+// let B = 8;
+// console.log(((A % 2 == 1) && (B % 2 == 0)) || ((A % 2 == 0) && (B % 2 == 1)));
+
+// --- Boolean-7 ---
+// let A = 5;
+// let B = 8;
+// let C = 10
+// console.log((A > 0)&&(B > 0)&&(C > 0));
+
+// --- Boolean-8 ---
+// let A = 5;
+// let B = 8;
+// let C = 10
+// console.log(((A < 0)&&(B < 0)&&(C > 0)) || ((A < 0)&&(B > 0)&&(C < 0)) || ((A > 0)&&(B < 0)&&(C < 0)));
+
+// --- Boolean-9 ---
+// let A = 5;
+// let B = 8;
+// let C = 10
+// console.log(((A < 0)&&(B > 0)&&(C > 0)) || ((A > 0)&&(B > 0)&&(C < 0)) || ((A > 0)&&(B < 0)&&(C > 0)));
+
+// --- Boolean-10 ---
+// let A = 5;
+// console.log((A / 10 >= 1) && (A%2==0));
+
+// --- Boolean-11 ---
+// let A = 5;
+// console.log((A / 100 >= 1) && (A % 2 == 1));
+
+// --- Boolean-12 ---
+// let A = 5;
+// let B = 8;
+// let C = 10
+// console.log((A - B == 0) || (A - C == 0) || (B - C == 0));
+
+// --- Boolean-13 ---
+// let A = 5;
+// let B = 8;
+// console.log();
+
+// --- Boolean-14 ---
+// let ABC = 5;
+// let C = ABC % 10;
+// let B = (ABC % 100) - C;
+// let A = Math.trunc(ABC / 100);
+// console.log(A < B < C);
+
+// --- Boolean-15 ---
+// let ABC = 5;
+// let C = ABC % 10;
+// let B = (ABC % 100) - c;
+// let A = Math.trunc(ABC / 100);
+// console.log((A < B < C) || (A > B && A > C));
+
---
+++ Section 2/main.js
@@ -0,0 +1,46 @@
+// ----------- FORMULALAR -----------
+
+// let A = 3; 
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// --- Formula-1 ---
+// let Y1 = (-B/(2*(A**2+B**2)));
+// console.log(Y1);
+
+// --- Formula-2 ---
+// let Y2 = ((A+(B/C)+D)**2)+B**2;
+// console.log(Y2);
+
+// --- Formula-3 ---
+// let Y3 = 2 * ((A**D) + ((4*C**2)/3));
+// console.log(Y3);
+
+// --- Formula-4 ---
+// let Y4 = (1/(A**2)) * ((B/10)**3) * ((C+D)**2);
+// console.log(Y4);
+
+// --- Formula-5 ---
+// let Y5 = ((A/B - 1) * ((C/(D-1))**2));
+// console.log(Y5);
+
+// --- Formula-6 ---
+// let Y6 = A + (B/C) + (D/A) + C;
+// console.log(Y6);
+
+// --- Formula-7 ---
+// let Y7 = Math.sqrt(((A+B+C)**2) - ((A-B-C)**2));
+// console.log(Y7);
+
+// --- Formula-8 ---
+// let Y8 = (A**2 + B**2 + C**2)/(A*B*C);
+// console.log(Y8);
+
+// --- Formula-9 ---
+// let Y9 = ((A*D**3)/3) + ((B*C**2)/2);
+// console.log(Y9);
+
+// --- Formula-10 ---
+// let Y10 = Math.abs((A/B)-(B/C)) + (4*Math.sqrt(A+C));
+// console.log(Y10); 
